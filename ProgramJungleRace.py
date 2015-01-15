2c432e7fecd211ce66c1522b3c258b5b5873b756
MarineJ <marine.julieron@ensc.fr>
2015-01-15 17:44:47 +0100

    maintenant ça marche plus haha

--- ProgramJungleRace.py
+++ ProgramJungleRace.py
@@ -36,6 +36,10 @@ weather = 2 # 0 == sunny, 1 == couldy, 2 == rainy, 3 == stormy
 nest = []
 food = []
 road = []
+regret = [	[ 36., 31., 28., 41.],
+			[ 36., 31., 28., 41.],
+			[ 36., 31., 28., 41.],
+			[ 36., 31., 28., 41.]]
 
 def update_whether():
 	print "Changng weather"
@@ -69,7 +73,7 @@ def one_Ant_departure():
 def update_all_ants_position():
 	if road:
 		i=len(road)-1
-		while i>0:
+		while i>=0:
 			road[i].update_position()
 			i-=1
 
@@ -127,6 +131,7 @@ class raceMap(object):
 	def innondations(self):
 		"""uptade the map to display a water at the given positions"""
 		for i in range(4):
+			self.raceMapGrid[13+i,1] = 4
 			screen.blit(self.water,((13+i)*32,1*32))
 		pygame.display.update()
 
@@ -144,22 +149,32 @@ class raceMap(object):
 	def trigger_events(self):
 		global weather
 		if weather == 0:
+			self.reset_events()
 			self.melted_tar()
 		elif weather == 1:
+			self.reset_events()
 			self.predator()
 		elif weather == 2:
+			self.reset_events()
 			self.innondations()
 		else:
 			self.tree_on_road()
+
+	def reset_events(self):
+		for i in range(4):
+			self.raceMapGrid[13+i,1] = 3
+			screen.blit(self.road,((13+i)*32,1*32))
+			pygame.display.update()
+
 
 ###############################################################
 # classe for ants
 ###############################################################
 
 class Ant:
-	modelProba = np.ones(4)
-	modelProba *= 1./4.;
 	def __init__(self, image, x0,y0):
+		self.quality = np.ones([4,4])*100
+		self.reward=100
 		self.image=image
 		self.x = x0
 		self.y = y0
@@ -167,13 +182,65 @@ class Ant:
 		self.previousY = self.y
 		self.road_number = -1
 		self.departure_whether = 0
+		self.carrying_food = 0
 
 	def update_position(self):
 		tmpx = self.x	
 		tmpy = self.y
-		if self.x<0 or self.x>19 or self.y<0 or self.y>28:
-			road.remove(self)
+		possible_step = []
+		# create a list with all next possibles position exept the previous one
+		if self.x-1>0 and self.x-1 != self.previousX:
+			possible_step.append([self.x-1,self.y])
+		if self.x+1<19 and self.x+1 != self.previousX:
+			possible_step.append([self.x+1,self.y])
+		if self.y-1>0 and self.y-1 != self.previousY :
+			possible_step.append([self.x,self.y-1])
+		if self.y+1<27 and self.y+1 != self.previousY:
+			possible_step.append([self.x,self.y+1])
+		print "length of possible step", len(possible_step)
+		# eliminate grass
+		for i in range(len(possible_step)-1):
+			if jungleRaceMap.raceMapGrid[possible_step[i][0],possible_step[i][1]]==1:
+				possible_step.pop(i)
+				i-=1
+		# determine wheter next step is road , obstacle, nest or food
+		print "position", possible_step[0][0],  possible_step[0][1]
+		if jungleRaceMap.raceMapGrid[possible_step[0][0],possible_step[0][1]]==3:
+			print "go farther"
+			self.x = possible_step[0][0]
+			self.y = possible_step[0][1]
+			self.previousX = tmpx
+			self.previousY = tmpy
+			self.reward-=1
+			self.display_ant()
+		elif jungleRaceMap.raceMapGrid[possible_step[0][0],possible_step[0][1]]==4:
+			print "go backward"
+			self.x = self.previousX
+			self.y = self.previousY
+			self.previousX = tmpx
+			self.previousY = tmpy
+			self.reward-=1
+			self.display_ant()
+		elif jungleRaceMap.raceMapGrid[possible_step[0][0],possible_step[0][1]]==8:
+			print "Reached food"
 			food.append(self)
+			road.remove(self)
+			self.undisplay_ant()
+			if self.carrying_food==0:
+				self.reward+=100
+			self.carrying_food = 1
+		else:
+			print "Reached nest"
+			nest.append(self)
+			road.remove(self)
+			self.undisplay_ant()
+			if self.carrying_food==1:
+				self.reward+=100
+			self.carrying_food = 0
+
+
+
+		"""if self.x<0 or self.x>19 or self.y<0 or self.y>28:
 			print self.x, self.y
 			return
 		if self.x>0 and self.x-1 != self.previousX and jungleRaceMap.raceMapGrid[self.x-1, self.y]==3:
@@ -184,26 +251,35 @@ class Ant:
 			self.y-=1
 		elif self.y<28 and self.y+1 != self.previousY and jungleRaceMap.raceMapGrid[self.x, self.y+1]==3:
 			self.y+=1 
-		else: # sinon on est arrivé
+		elif self.y>24:
+			print "Reached food"
 			food.append(self)
 			road.remove(self)
-			self.x=9
-			self.y=27
+			return
+		elif self.y<4:
+			print "Reached nest"
+			nest.append(self)
+			road.remove(self)
 			return
 		if tmpx!=self.x or tmpy!=self.y:
 			self.previousX = tmpx
 			self.previousY = tmpy
-		self.display_ant()
+		self.reward-=1
+		self.display_ant()"""
 
 	def chose_one_way(self,GorC):
 		# random pick a way regarding the model
-		# beta = random.random()
-		# way_num = 0
-		# accu = 0
-		# while beta<accu:
-		# 	accu+= modelProba[weather,way_num]
-		# 	way_num+=1
-		way_num=random.randint(0,3)
+		choices=self.quality[weather,:]
+		max_quality=choices[0]
+		eq_choices=[]
+		for i in range(len(choices)):
+			if max_quality < choices[i]:
+				max_quality = choices[i]
+		for i in range(len(choices)):
+			if choices[i]==max_quality:
+				eq_choices.append(i)
+		index_way = random.randint(0,len(eq_choices)-1)
+		way_num = eq_choices[index_way]
 		self.road_number = way_num
 		road.append(self)
 		if GorC==0:
@@ -230,28 +306,36 @@ class Ant:
 			else:
 				self.x = 11
 				self.y = 0
+			self.previousX = self.x
+			self.previousY = self.y
 		else:
 			if way_num == 0:
-				self.x = 26
-				self.y = 6
+				self.y = 27
+				self.x = 5
 			elif way_num == 1:
-				self.x = 25
-				self.y = 6
+				self.y = 25
+				self.x = 6
 			elif way_num == 2:
-				self.x = 25
-				self.y = 8
+				self.y = 25
+				self.x = 8
 			else:
-				self.x = 26
-				self.y = 9
+				self.y = 26
+				self.x = 9
+			self.previousX = self.x
+			self.previousY = self.y
 
 	def display_ant(self):
 		screen.blit(jungleRaceMap.road,(self.previousY*32,self.previousX*32))  
 		screen.blit(image_ant,(self.y*32,self.x*32))
 
+	def undisplay_ant(self):
+		screen.blit(jungleRaceMap.road,(self.previousY*32,self.previousX*32)) 
+
 	def chose_one_return_way(self):
 		pass
 
-	def update_model(self):
+	def update_model(self, way_num):
+		self.quality[wwl_nest, wnwl_nest] += learning_rate*(self.reward + self.disount* - self.quality[wwl_nest, wnwl_nest])
 		pass
 
 
@@ -270,7 +354,7 @@ try:
 	print "Ants creation"
 	# create and put all ants in the nest
 
-	for i in range(10):
+	for i in range(1):
 		nest.append(Ant(image_ant,8,1))
 
 
@@ -293,7 +377,7 @@ try:
 		one_Ant_departure()
 		jungleRaceMap.trigger_events()
 		for i in range(len(road)):
-			road[i].update_model()
+			#road[i].update_model()
 			pass
 		count+=1
 		if count == 20:
